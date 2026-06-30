359be13b32bb941e1d5258f1f4d19bf010e25e2f
Salmon <tankoon320@gmail.com>
2026-06-30 22:51:45 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# main.py
+
+# Import ระบบต่างๆ ที่เพื่อนในกลุ่มเขียนไว้
+from features import recruitment, database, finance, missions
+
+# ข้อมูลตั้งต้น (Initial Data) สำหรับเอาไว้เทสต์โปรแกรม
+family_members = [
+    {"name": "Tony", "age": 35, "role": "Boss", "power": 10, "money": 5000000},
+    {"name": "Luigi", "age": 28, "role": "Hitman", "power": 8, "money": 150000},
+    {"name": "Mario", "age": 22, "role": "Slave", "power": 4, "money": 500}
+]
