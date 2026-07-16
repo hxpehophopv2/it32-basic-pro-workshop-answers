00b2359ddad6b73d658b87b797a661536d71faf5
Teeruch Songtalay <maieedeed@gmail.com>
2026-07-16 23:37:27 +0700

    update workshop 4 to have menu system

--- workshop-4/workshop-4.py
+++ workshop-4/workshop-4.py
@@ -1,10 +1,10 @@
 # WORKSHOP 4 - ระบบบันทึกข้อมูลลูกน้องคนใหม่
 
-def add_member(name, age, money):
+def add_member(name, age, gang):
     new_member = {
         "name": name,
         "age": age,
-        "money": money
+        "gang": gang
     }
     gang_members.append(new_member)
     return new_member
@@ -12,16 +12,18 @@ def add_member(name, age, money):
 gang_members = []
 
 while True:
-    name_input = input()
-    if name_input == "q":
+    choice = input("1 for Add member | 2 for Show member | [Any key] Quit: ")
+    if choice == "1":
+        name_input = input("Enter you name: ")
+        age_input = int(input("Enter your age: "))
+        gang_input = input("Enter your gang: ")
+
+        add_member(name_input, age_input, gang_input)
+        print("Add member successfully!")
+    elif choice == "2":
+        print("GANG MEMBERS:")
+        print(gang_members)
+    else:
         print("Quitting...")
         break
-    
-    age_input = int(input())
-    money_input = float(input())
-
-    add_member(name_input, age_input, money_input)
-    print("Add member successfully!")
-
-print("GANG MEMBERS:")
-print(gang_members)
+        
